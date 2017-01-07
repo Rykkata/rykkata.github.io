6b95b5f1f297773f43483cf01122ec9d675ac45d
Nicholas Stuter <nstuter@gmail.com>
2017-01-07 18:33:18 -0500

    Sample asp

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rykkata.github.io.Startup))]
+namespace rykkata.github.io
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
